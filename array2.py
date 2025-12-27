f006f93b31a9eb849ae334e22a969a4be55f2410
بهاره نوریان <bahar.coder3@gmail.com>
2025-12-27 20:29:41 +0330

    از کاربر پنج عدد بگیر و داخل لیست ذخیره وچاپ کن

---
+++ array2.py
@@ -0,0 +1,8 @@
+
+#از کاربر پنج عدد بگیر و داخل لیست ذخیره وچاپ کن
+
+numbers=[]
+for i in range(6):
+      num =int (input("enter number{i+1}:"))
+      numbers.append(num)
+      print("your numbers:",numbers)
